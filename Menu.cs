de7c68ff1d4eb26d679600d6bbec821414cdef18
MischaTobias <mischa.tobias4b@gmail.com>
2021-04-26 16:11:42 -0600

    Project Creation

---
+++ Menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TuringMachineExecuter
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
