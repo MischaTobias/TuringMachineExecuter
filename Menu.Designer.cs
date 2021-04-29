bd1588f94188a76fd05142507ae81f8df26d2ab3
MischaTobias <mischa.tobias4b@gmail.com>
2021-04-29 12:29:56 -0600

    Replaced DataGrid with TLayoutPanel

--- Menu.Designer.cs
+++ Menu.Designer.cs
@@ -37,13 +37,12 @@ namespace TuringMachineExecuter
             this.btnNextStep = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.lblTuringFile = new System.Windows.Forms.Label();
-            this.dgvCinta = new System.Windows.Forms.DataGridView();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.lblCurrentNode = new System.Windows.Forms.Label();
             this.lblLastMovement = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvCinta)).BeginInit();
+            this.tlpCinta = new System.Windows.Forms.TableLayoutPanel();
             this.SuspendLayout();
             // 
             // btnLoadFile
@@ -116,20 +115,6 @@ namespace TuringMachineExecuter
             this.lblTuringFile.TabIndex = 6;
             this.lblTuringFile.Text = "-";
             // 
-            // dgvCinta
-            // 
-            this.dgvCinta.AllowUserToAddRows = false;
-            this.dgvCinta.AllowUserToDeleteRows = false;
-            this.dgvCinta.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.dgvCinta.BackgroundColor = System.Drawing.SystemColors.ControlLight;
-            this.dgvCinta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvCinta.Location = new System.Drawing.Point(207, 190);
-            this.dgvCinta.Name = "dgvCinta";
-            this.dgvCinta.ReadOnly = true;
-            this.dgvCinta.RowTemplate.Height = 25;
-            this.dgvCinta.Size = new System.Drawing.Size(514, 90);
-            this.dgvCinta.TabIndex = 7;
-            // 
             // label2
             // 
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
@@ -168,7 +153,7 @@ namespace TuringMachineExecuter
             this.lblLastMovement.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblLastMovement.AutoSize = true;
             this.lblLastMovement.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblLastMovement.Location = new System.Drawing.Point(452, 346);
+            this.lblLastMovement.Location = new System.Drawing.Point(451, 360);
             this.lblLastMovement.Name = "lblLastMovement";
             this.lblLastMovement.Size = new System.Drawing.Size(15, 19);
             this.lblLastMovement.TabIndex = 12;
@@ -179,11 +164,27 @@ namespace TuringMachineExecuter
             this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label6.Location = new System.Drawing.Point(301, 300);
+            this.label6.Location = new System.Drawing.Point(300, 314);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(318, 21);
             this.label6.TabIndex = 11;
             this.label6.Text = "Última transición/movimiento realizado";
+            // 
+            // tlpCinta
+            // 
+            this.tlpCinta.ColumnCount = 5;
+            this.tlpCinta.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tlpCinta.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tlpCinta.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tlpCinta.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tlpCinta.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tlpCinta.Location = new System.Drawing.Point(218, 193);
+            this.tlpCinta.Name = "tlpCinta";
+            this.tlpCinta.RowCount = 2;
+            this.tlpCinta.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpCinta.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpCinta.Size = new System.Drawing.Size(487, 100);
+            this.tlpCinta.TabIndex = 13;
             // 
             // menuForm
             // 
@@ -191,12 +192,12 @@ namespace TuringMachineExecuter
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(759, 450);
+            this.Controls.Add(this.tlpCinta);
             this.Controls.Add(this.lblLastMovement);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.lblCurrentNode);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.dgvCinta);
             this.Controls.Add(this.lblTuringFile);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnNextStep);
@@ -208,7 +209,6 @@ namespace TuringMachineExecuter
             this.Name = "menuForm";
             this.Text = "TM Executer";
             this.Load += new System.EventHandler(this.Menu_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvCinta)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -223,11 +223,11 @@ namespace TuringMachineExecuter
         private System.Windows.Forms.Button btnNextStep;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblTuringFile;
-        private System.Windows.Forms.DataGridView dgvCinta;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label lblCurrentNode;
         private System.Windows.Forms.Label lblLastMovement;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TableLayoutPanel tlpCinta;
     }
 }
